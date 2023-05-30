c704cffceeedfde1e941d642d52dda4c9f47ec53
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-05-30 16:28:59 -0700

    will show results of the function to the user with a clear message now.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,9 @@ function add (number1, number2){
 let number1 = parseInt(prompt("Please Enter Your First Number:"));
 let number2 = parseInt(prompt("Please Enter Your Second Number:"));
 
-window.alert(add(number1, number2));
+let newNum = add(number1, number2);
+window.alert("Your new number is " + newNum);
+
 
 function subtract(num1, num2) {
   return num1 - num2;
@@ -13,8 +15,8 @@ function subtract(num1, num2) {
 
 let num1 = prompt("Enter a number:");
 let num2 = prompt("Enter another number:");
-
-window.alert(subtract(num1, num2));
+let result = subtract(num1, num2);
+window.alert("Your new number is " + result);
 
 function mulitply(numbers1, numbers2) {
     return numbers1 * numbers2;
@@ -22,5 +24,14 @@ function mulitply(numbers1, numbers2) {
   
   let numbers1 = prompt("Enter a number:");
   let numbers2 = prompt("Enter another number:");
+  let numbers 3 = multiply(numbers1,numbers2)
+  window.alert("Your new number is" + numbers3)
+
+  function divide(numero1, numero2){
+    return numero1 / numero2;
+  }
   
-  window.alert(multiply(numbers1, numbers2));
+  let numero1 = parseInt(prompt("Please Enter Your First Number:"));
+  let numero2 = parseInt(prompt("Please Enter Your Second Number:"));
+  
+  window.alert(divide(numero1, numero2));
