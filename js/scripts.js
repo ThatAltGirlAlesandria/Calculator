8eb97485669f1f8e7eb5a0d10f07e0bc4d61311d
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-05-30 16:07:59 -0700

    Created subtraction function. Co-authored-by: Jase Grable <jase.grable@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,21 @@ let number1 = parseInt(prompt("Please Enter Your First Number:"));
 let number2 = parseInt(prompt("Please Enter Your Second Number:"));
 
 window.alert(add(number1, number2));
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+let num1 = prompt("Enter a number:");
+let num2 = prompt("Enter another number:");
+
+window.alert(subtract(num1, num2));
+
+function mulitply(num1, num2) {
+    return num1 * num2;
+  }
+  
+  let num1 = prompt("Enter a number:");
+  let num2 = prompt("Enter another number:");
+  
+  window.alert(multiply(num1, num2));
