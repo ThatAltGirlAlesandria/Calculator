0dedf970982139f7b2515951cddb28cac912de04
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-05-30 16:34:55 -0700

    all functions now have a 'new number' prompt.

--- js/scripts.js
+++ js/scripts.js
@@ -18,20 +18,21 @@ let num2 = prompt("Enter another number:");
 let result = subtract(num1, num2);
 window.alert("Your new number is " + result);
 
-function mulitply(numbers1, numbers2) {
+function multiply(numbers1, numbers2) {
     return numbers1 * numbers2;
   }
   
   let numbers1 = prompt("Enter a number:");
   let numbers2 = prompt("Enter another number:");
-  let numbers 3 = multiply(numbers1,numbers2)
-  window.alert("Your new number is" + numbers3)
+  let numbers3 = multiply(numbers1, numbers2)
+  window.alert("Your new number is " + numbers3)
 
-  function divide(numero1, numero2){
+function divide(numero1, numero2){
     return numero1 / numero2;
   }
   
-  let numero1 = parseInt(prompt("Please Enter Your First Number:"));
-  let numero2 = parseInt(prompt("Please Enter Your Second Number:"));
+  let numero1 = prompt("Enter a Number:");
+  let numero2 = prompt("Enter a Number:");
+  let newNumber = divide(numero1, numero2)
   
-  window.alert(divide(numero1, numero2));
+  window.alert("Your new number is " + newNumber);
